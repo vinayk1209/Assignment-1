3977998183ef189ac96bedddedc0bed62aa9903c
G. Vinay Kumar <47490861+vinayk1209@users.noreply.github.com>
2022-10-23 16:43:17 +0530

    1st Assignment

---
+++ Q_3.java
@@ -0,0 +1,19 @@
+
+public class Q_3 {
+
+	public static void main(String[] args) {
+		int n = 13;
+		for(int i=0; i< n; i++) {
+			for(int j=0; j<n; j++) {
+				if((i+j)<=(n-1)/2 || (j-i)>=(n-1)/2 || i==(n-1) || j==0 || j==(n-1))
+					System.out.print('*');
+				else 
+					System.out.print(' ');
+			}
+			System.out.println();
+		}
+
+
+	}
+
+}
---
+++ Q_4.java
@@ -0,0 +1,18 @@
+
+public class Q_4 {
+
+	public static void main(String[] args) {
+		int n = 14;
+		for(int i=0; i< n; i++) {
+			for(int j=0; j<n; j++) {
+				if((i-j)>=(n-1)/2 || (i+j) >= n+((n-1)/2)-1)
+					System.out.print('*');
+				else 
+					System.out.print(' ');
+			}
+			System.out.println();
+		}
+
+	}
+
+}
